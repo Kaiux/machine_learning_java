9b233dc1eaebba3c5a13b19a46ef3a053d2d25fb
TechieYu <kaiux@foxmail.com>
2024-08-25 18:05:38 +0800

    simple linear regression

---
+++ example/src/main/java/cn/kaiux/machine_learning/example/linear_regression/SimpleLinearRegression.java
@@ -0,0 +1,13 @@
+package cn.kaiux.machine_learning.example.linear_regression;
+
+/**
+ * @author TechieYu
+ * @date 2024/8/25
+ */
+public class SimpleLinearRegression {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ math/src/main/java/cn/kaiux/machine_learning/math/Matrix.java
@@ -0,0 +1,8 @@
+package cn.kaiux.machine_learning.math;
+
+/**
+ * @author TechieYu
+ * @date 2024/8/25
+ */
+public class Matrix {
+}
